67e0432055f39fef364175acffe219d5fadf600d
Paige Kelly <pckelly.co@gmail.com>
2021-01-30 00:58:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,34 +8,16 @@ def join_nested_strings(src)
   words = []
   row_idx = 0
   while row_idx < src.length do
-    sub_array =  src[row_idx]
+    row_idx += 1
+    sub_arrqy = src[row_idx]
     ele_idx = 0
-    while ele_idx < sub_array.length do
-      ele = sub_array[ele_idx]
-      if ele.is_a? String do
+    while ele_idx < sub_arrqy.length do
+      ele_idx += 1
+      ele = sub_arrqy[ele_idx]
+      if ele.is_a? String 
         words.push(ele)
       end
-      ele_idx += 1
-    end 
-    row_idx += 1
     end
-  words.join(' ')
-end
-
-def join_nested_strings(src)
-  words = []
-  row_idx = 0
-  while row_idx < src.length {
-    sub_array =  src[row_idx]
-    ele_idx = 0
-    row_idx += 1
-   while ele_idx < sub_array.length {
-    ele = sub_array[ele_idx]
-    ele_idx += 1
-    if ele.is_a? String {
-      words.push(ele)
-    }
-  }
-  
-  words.join(' ')
+  end
+  words.join(" ")
 end
