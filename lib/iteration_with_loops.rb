09675e6b1098d9856e4394b1dd8e3f3c7b776433
Paige Kelly <pckelly.co@gmail.com>
2021-01-30 01:03:08 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,23 @@
+# identify each sub-array
+# identify each ele in sub-array
+# compare ele in sub-array to see if the element is_a? String
+# if ele is_a? String, push ele to new array
+# return new array and .join with space
+
 def join_nested_strings(src)
-  # src will be an Array of Arrays of Strings and Integers
-  # Combine all Strings present in the AoA into a single value and return it
+  words = []
+  row_idx = 0
+  while row_idx < src.length do
+    sub_array = src[row_idx]
+    row_idx += 1
+    ele_idx = 0
+    while ele_idx < sub_array.count do
+      ele = sub_array[ele_idx]
+      if ele.is_a? String 
+        words.push(ele)
+      end
+      ele_idx += 1
+    end
+  end
+  words.join(" ")
 end
